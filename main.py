4efb8b94149f4807e3f39b41098a2de48e248112
nowikens <kuban96166@gmail.com>
2020-07-06 22:14:37 +0200

    strategies classes

--- main.py
+++ main.py
@@ -1,1 +1,90 @@
-print("hello world")
+import random
+
+
+
+
+# Prisoner's dillema game
+
+
+class Defective_player():
+    def __init__(self):
+        self.status = 'defect'
+        self.points = 0
+    
+    def strategy(self):
+        self.status = 'defect'
+
+class Cooperative_player():
+    def __init__(self):
+        self.status = 'cooperate'
+        self.points = 0
+    
+    def strategy(self):
+        self.status = 'cooperate'
+
+class Vetzavet():
+    def __init__(self):
+        self.status = 'cooperate'
+        self.points = 0
+        self.last_gain = []
+    
+    def strategy(self):
+        if self.last_gain == []:
+            self.status = 'cooperate'
+            self.last_gain = [self.points]
+        elif self.last_gain[-1] == 0:
+            self.status = 'defective'
+            self.last_gain.append(self.points-sum(self.last_gain))
+        elif self.last_gain[-1] == 1:
+            self.status = 'defective'
+            self.last_gain.append(self.points-sum(self.last_gain))
+        elif self.last_gain == 3:
+            self.status = 'cooperate'
+            self.last_gain.append(self.points-sum(self.last_gain))
+        elif self.last_gain == 5:
+            self.status = 'cooperate'
+            self.last_gain.append(self.points-sum(self.last_gain))
+
+
+class Random():
+    possible_status = ('defect', 'cooperate')
+    def __init__(self):
+        self.status = random.choice(possible_status)
+        
+    def strategy(self):
+        self.status = random.choice(possible_status)
+        
+        
+player1 = Vetzavet()
+
+player1.points = 3
+player1.strategy()
+print(player1.status)
+print(player1.points)
+print(player1.last_gain)
+
+player1.points += 1
+player1.strategy()
+print(player1.status)
+print(player1.points)
+print(player1.last_gain)
+
+player1.points += 5
+player1.strategy()
+print(player1.status)
+print(player1.points)
+print(player1.last_gain)
+
+
+player1.points += 3
+player1.strategy()
+print(player1.status)
+print(player1.points)
+print(player1.last_gain)
+
+
+player1.points += 0
+player1.strategy()
+print(player1.status)
+print(player1.points)
+print(player1.last_gain)
