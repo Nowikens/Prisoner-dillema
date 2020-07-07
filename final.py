1c40e91366bf2591a09546fee25070019c95df5f
Nevilter <67866919+Nevilter@users.noreply.github.com>
2020-07-07 22:15:57 +0200

    Update final.py
    Dodałem Klasę Player, jako klasę nadrzędną dla wszystkich rodzajów graczy
    Zmieniłem funkcję Play, żeby wywoływała atrybuty Playerów, zamiast tworzyć własne

--- final.py
+++ final.py
@@ -5,38 +5,44 @@ import random
 
 # -------------------------------------- PLAYERS -------------------- #
 
-
-class DefectivePlayer():
+class Player():
+    def __init__(self):
+        self.points = 0
+        self.gain = 0
+    
+class DefectivePlayer(Player):
     """
         This player always is defective
     """
     def __init__(self):
+        super(Player, self).__init__()
         self.status = 'defect'
-        self.points = 0
     
     def strategy(self):
         self.status = 'defect'
 
-class CooperativePlayer():
+class CooperativePlayer(Player):
     """
         This player always is cooperative
     """
     def __init__(self):
+        super(Player, self).__init__()
         self.status = 'cooperate'
-        self.points = 0
+        
     
     def strategy(self):
         self.status = 'cooperate'
 
-class TitForTatPlayer():
+class TitForTatPlayer(Player):
     """
         This player answers to other player's moves.
         If other player cooperates, this player cooperates in next move.
         If other player defects, this player defects in next move.
     """
     def __init__(self):
+        super(Player, self).__init__()
         self.status = 'cooperate'
-        self.points = 0
+        
         
         # last_gain is a list, that holds gains from previous moves
         self.last_gain = []
@@ -56,9 +62,10 @@ class TitForTatPlayer():
             self.last_gain.append(self.points-sum(self.last_gain))
 
 
-class RandomPlayer():
+class RandomPlayer(Player):
     
     def __init__(self):
+        super(Player, self).__init__()
         self.possible_status = ('defect', 'cooperate')
         self.status = random.choice(self.possible_status)
         self.points = 0
@@ -84,28 +91,28 @@ class Game():
         
     def play(self, rounds):
         for r in range(rounds):
-            if self.player1.status == "cooperate" and self.player2.status == "cooperate":
-                self.player1_gain = 3
-                self.player2_gain = 3
-            elif self.player1.status == "cooperate" and self.player2.status == "defect":
-                self.player1_gain = 0
-                self.player2_gain = 5
-            elif self.player1.status == "defect" and self.player2.status == "cooperate":
-                self.player1_gain = 5
-                self.player2_gain = 0
-            else:
-                self.player1_gain = 1
-                self.player2_gain = 1
-            
-            self.player1.points += self.player1_gain
-            self.player2.points += self.player2_gain
-            self.player1.strategy()
-            self.player2.strategy()
             print("\nRound ", r)
             print("\nPLAYER1 chose to " , self.player1.status)
             print("\nPLAYER2 chose to " , self.player2.status)
-            print("\nPLAYER1 gains ", self.player1_gain, " point, and now has ", self.player1.points, " points")
-            print("\nPLAYER2 gains ", self.player2_gain, " point, and now has ", self.player2.points, " points")
+            if self.player1.status == "cooperate" and self.player2.status == "cooperate":
+                player1.gain = 3
+                player2.gain = 3
+            elif self.player1.status == "cooperate" and self.player2.status == "defect":
+                player1.gain = 0
+                player2.gain = 5
+            elif self.player1.status == "defect" and self.player2.status == "cooperate":
+                player1.gain = 5
+                player2.gain = 0
+            else:
+                player1.gain = 1
+                player2.gain = 1
+            
+            player1.points += player1.gain
+            player2.points += player2.gain
+            player1.strategy()
+            player2.strategy()          
+            print("\nPLAYER1 gains ", player1.gain, " point, and now has ", player1.points, " points")
+            print("\nPLAYER2 gains ", player2.gain, " point, and now has ", player2.points, " points")
 
     
 def main():
